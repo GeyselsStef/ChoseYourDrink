02b9012530f3364d9d2965925a442d3ee6f1b735
Stef Geysels <programming@geyselstef.be>
2024-06-28 09:55:09 +0200

    Test Automapper again

--- Source/ChoseYourDrink/Program.cs
+++ Source/ChoseYourDrink/Program.cs
@@ -10,19 +10,22 @@ using Blazored.Toast;
 using Mapper;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<DrinksHttpClient>();
-builder.Services.AddScoped<OrderApiHttpClient>();
 builder.Services.AddScoped<ChoseYourDrink.Services.IDrinkService, ChoseYourDrink.Services.DrinkService>();
-builder.Services.AddScoped<ChoseYourDrink.BLL.IDrinkService, ChoseYourDrink.BLL.DrinkService>();
 builder.Services.AddSingleton<IUserService, UserService>();
 builder.Services.AddTransient<IJSRunitmeServices, JSRunitmeServices>();
 
+builder.Services.AddBLLServices();
+
 builder.Services.AddBlazoredModal();
 builder.Services.AddBlazoredToast();
+
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 
 builder.Services.AddMapper();
 
